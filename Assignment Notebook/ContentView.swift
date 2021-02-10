c1b0e9d1e30f12d411cd8975c13551f6bedba8ff
Darren Darlington <darrend42@outlook.com>
2021-02-10 10:20:52 -0600

    Added array and Courses with descriptions

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var assignmentItem = [AssignmentItem(course: "History", description: "Learn about past", dueDate: Date()),
+                                 AssignmentItem(course: "Math", description: "Learn to use numbers", dueDate: Date()),
+                                AssignmentItem(course: "Computer Science", description: "Learn to code", dueDate: Date())]
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -18,4 +22,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
