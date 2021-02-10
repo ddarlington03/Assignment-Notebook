d9564546b3d33926272deea6673e6150cb5ed548
Darren Darlington <darrend42@outlook.com>
2021-02-10 17:51:57 -0600

    Added Course description and Dates

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -10,23 +10,39 @@ import SwiftUI
 struct ContentView: View {
     @State var assignmentItem = [AssignmentItem(course: "History", description: "Learn about past", dueDate: Date()),
                                  AssignmentItem(course: "Math", description: "Learn to use numbers", dueDate: Date()),
-                                AssignmentItem(course: "Computer Science", description: "Learn to code", dueDate: Date())]
+                                 AssignmentItem(course: "Computer Science", description: "Learn to code", dueDate: Date())]
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentItem) { item in
+                    HStack {
+                        VStack (alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .navigationBarTitle("Courses")
+            }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        
+        
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+        
+        struct AssignmentItem: Identifiable {
+            var id = UUID()
+            var course = String()
+            var description = String()
+            var dueDate = Date()
+        }
     }
-}
-
-struct AssignmentItem: Identifiable {
-    var id = UUID()
-    var course = String()
-    var description = String()
-    var dueDate = Date()
-}
